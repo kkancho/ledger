fa48731a5f45cdfeafb8bc9cdef3eede80049351
kkancho <46648614+kkancho@users.noreply.github.com>
2026-07-30 20:34:00 +0900

    기존 종목 사다리 LOC 자동 보정
    저장된 데이터의 사다리 주문방식이 기본값 변경에 반영되지 않던 문제 해결. 1회성 보정으로 처리하고 이후 사용자 변경은 존중, sw 캐시 v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'saeam-ledger-v13';
+const CACHE = 'saeam-ledger-v14';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
