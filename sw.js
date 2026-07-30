97f5dae2eef95f935b310911da4a7542dff532e6
kkancho <46648614+kkancho@users.noreply.github.com>
2026-07-30 13:12:48 +0900

    수수료.제세금 보정 추가
    수수료.제세금 보정 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'saeam-ledger-v5';
+const CACHE = 'saeam-ledger-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
