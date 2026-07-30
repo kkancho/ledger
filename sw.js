058d5333968bffb4bcfd16fff2921365dfca807c
kkancho <46648614+kkancho@users.noreply.github.com>
2026-07-30 20:24:30 +0900

    사다리 주문방식 LOC로 수정
    V4.0 원문은 매수 전부 LOC. 사다리 기본값을 지정가에서 LOC로 정정하고 이유 설명 추가, sw 캐시 v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'saeam-ledger-v12';
+const CACHE = 'saeam-ledger-v13';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
