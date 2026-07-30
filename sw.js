b40cfcee631c2cab792cd2a4df299d4b53849962
kkancho <46648614+kkancho@users.noreply.github.com>
2026-07-30 12:51:07 +0900

    분할매수 계산기 추가
    개인 모드에 범용 분할매수 계산기 탭 추가, sw 캐시 v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'saeam-ledger-v3';
+const CACHE = 'saeam-ledger-v5';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
